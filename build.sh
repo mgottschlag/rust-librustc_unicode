a729a959080e5c0e1ea3b80b2ef712e3c6cbe4ac
Vladimir Pouzanov <farcaller@gmail.com>
2015-05-29 11:52:57 +0100

    Added support for curl

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 mkdir -p rust
 cd rust
-wget -q -O rust.tar.gz https://github.com/rust-lang/rust/tarball/`rustc --version|awk '{sub(/\\(/, "", $3); print $3}'`
+
+DOWNLOAD_LINK="https://github.com/rust-lang/rust/tarball/`rustc --version|awk '{sub(/\\(/, "", $3); print $3}'`"
+
+which wget >/dev/null 2>/dev/null
+if [ $? -eq 0 ]; then
+  wget -q -O rust.tar.gz "$DOWNLOAD_LINK"
+else
+  which curl >/dev/null 2>/dev/null
+  if [ $? -eq 0 ]; then
+   curl -o rust.tar.gz "$DOWNLOAD_LINK"
+  else
+    echo "wget or curl required"
+    exit 1
+  fi
+fi
 tar -zx --strip-components=1 -f rust.tar.gz
