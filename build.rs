4bb5300680f783ec234c5322a59d713d97703634
Matt Coffin <mcoffin13@gmail.com>
2015-06-09 05:20:52 -0700

    Convert version-grabbing to rust

--- build.rs
+++ build.rs
@@ -1,10 +1,49 @@
 use std::process::Command;
+use std::env;
+use std::fs;
+use std::io;
+use std::path::Path;
+
+const RUST_DIR: &'static str = "./rust";
+
+fn ensure_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    let path = path.as_ref();
+    match fs::read_dir(path) {
+        Ok(..) => Ok(()),
+        Err(..) => fs::create_dir(path),
+    }
+}
 
 fn main() {
+    // Ensure the rust directory exists
+    if let Err(e) = ensure_dir(RUST_DIR) {
+        panic!(e);
+    }
+
+    // cd to the rust directory
+    if let Err(e) = env::set_current_dir(RUST_DIR) {
+        panic!(e);
+    }
+
+    // Run rustc to get the version
+    let rustc_output = Command::new("rustc").arg("--version")
+        .output().unwrap_or_else(|e| {
+            panic!("failed to execute rustc: {}", e);
+        });
+    let output_bytes: &[u8] = rustc_output.stdout.as_ref();
+    let version = match std::str::from_utf8(output_bytes) {
+        Ok(s) => s.split(" ").nth(2).expect("rustc gave invalid version format"),
+        Err(e) => panic!(e),
+    }.trim_left_matches("(");
+
     // Shell out to perform the build.  In the future, the logic
     // to grab libcore could be done in rust in order to support
     // platforms without a posix shell
-	Command::new("sh").arg("./build.sh").status().unwrap_or_else(|e| {
-		panic!("failed to execute process: {}", e)
-    });
+    Command::new("sh")
+        .arg("../build.sh")
+        .env("DOWNLOAD_LINK",
+             format!("https://github.com/rust-lang/rust/tarball/{}", version))
+        .status().unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e);
+        });
 }
--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-mkdir -p rust
-cd rust
-
-DOWNLOAD_LINK="https://github.com/rust-lang/rust/tarball/`rustc --version|awk '{sub(/\\(/, "", $3); print $3}'`"
-
 which wget >/dev/null 2>/dev/null
 if [ $? -eq 0 ]; then
   wget -q -O rust.tar.gz "$DOWNLOAD_LINK"
